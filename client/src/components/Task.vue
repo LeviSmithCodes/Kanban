5faa29f0c441fb40ff791eafe7f9f09893e6d2f6
LeviSmithCodes <eyevel@gmail.com>
2020-01-08 16:15:32 -0700

    More Stuff

--- client/src/components/Task.vue
+++ client/src/components/Task.vue
@@ -4,6 +4,25 @@
     <button class="btn btn-danger" @click="deleteTask(taskData._id)">
       <i class="fas fa-trash-alt"></i>
     </button>
+    <!--Dropdown primary-->
+    <!-- Basic dropdown -->
+    <button
+      class="btn btn-primary dropdown-toggle mr-4"
+      type="button"
+      data-toggle="dropdown"
+      aria-haspopup="true"
+      aria-expanded="false"
+    >Basic dropdown</button>
+
+    <div class="dropdown-menu">
+      <a class="dropdown-item" href="#">Action</a>
+      <a class="dropdown-item" href="#">Another action</a>
+      <a class="dropdown-item" href="#">Something else here</a>
+      <div class="dropdown-divider"></div>
+      <a class="dropdown-item" href="#">Separated link</a>
+    </div>
+    <!-- Basic dropdown -->
+    <!--/Dropdown primary-->
   </li>
   <!-- {{taskData}} -->
 </template>
