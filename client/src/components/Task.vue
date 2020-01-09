8b13f8a5dc076d4ee6a2299d9c6209f1fce9561b
LeviSmithCodes <eyevel@gmail.com>
2020-01-09 11:29:24 -0700

    Started on task moving dropdown

--- client/src/components/Task.vue
+++ client/src/components/Task.vue
@@ -1,28 +1,13 @@
 <template>
   <li>
     {{taskData.description}}
+    <select class="form-control" @change="changeTaskList($event)">
+      <option value selected disabled>Select List</option>
+      <option v-for="list in availableLists" :value="list.id" :key="list.id">{{ list.title }}</option>
+    </select>
     <button class="btn btn-danger" @click="deleteTask(taskData._id)">
       <i class="fas fa-trash-alt"></i>
     </button>
-    <!--Dropdown primary-->
-    <!-- Basic dropdown -->
-    <button
-      class="btn btn-primary dropdown-toggle mr-4"
-      type="button"
-      data-toggle="dropdown"
-      aria-haspopup="true"
-      aria-expanded="false"
-    >Basic dropdown</button>
-
-    <div class="dropdown-menu">
-      <a class="dropdown-item" href="#">Action</a>
-      <a class="dropdown-item" href="#">Another action</a>
-      <a class="dropdown-item" href="#">Something else here</a>
-      <div class="dropdown-divider"></div>
-      <a class="dropdown-item" href="#">Separated link</a>
-    </div>
-    <!-- Basic dropdown -->
-    <!--/Dropdown primary-->
   </li>
   <!-- {{taskData}} -->
 </template>
@@ -39,7 +24,14 @@ export default {
       newComment: {
         content: "",
         taskId: this.taskData.id
-      }
+      },
+      jobTitles: [
+        { name: "Product designer", id: 1 },
+        { name: "Full-stack developer", id: 2 },
+        { name: "Product manager", id: 3 },
+        { name: "Senior front-end developer", id: 4 }
+      ],
+      selectedList: null
     };
   },
   methods: {
@@ -48,6 +40,15 @@ export default {
         taskId: this.taskData.id,
         boardId: this.taskData.boardId
       });
+    },
+    changeTaskList(event) {
+      this.selectedList =
+        event.target.options[event.target.options.selectedIndex].text;
+    }
+  },
+  computed: {
+    availableLists() {
+      return this.$store.state.lists;
     }
   },
   props: ["taskData"]
